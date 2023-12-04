6a12c8c59d9b677c51957dced0a360cff4f62239
flylying <145457728+flylying@users.noreply.github.com>
2023-12-04 12:27:55 +0000

    new up

--- linguistics/__init__.py
+++ linguistics/__init__.py
@@ -1,3 +1,4 @@
 from .document import *
 from .tokenize_function import tokenize
-from .google impo
+from .google import GoogleCloud
+from .repustate import Repustate
---
+++ linguistics/bert/BertVectorizer.py
@@ -0,0 +1,5 @@
+from .BertVectorizerModel import BertVectorizerModel
+import torch
+
+
+class BertVect
