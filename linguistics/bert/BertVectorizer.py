3e1a6dae38da8f4ed68f4fb78b2e997b2f526005
flylying <145457728+flylying@users.noreply.github.com>
2023-12-04 18:07:55 +0000

    Back to SNAPSHOT

--- linguistics/bert/BertVectorizer.py
+++ linguistics/bert/BertVectorizer.py
@@ -2,4 +2,7 @@ from .BertVectorizerModel import BertVectorizerModel
 import torch
 
 
-class BertVect
+class BertVectorizer:
+	def __init__(self, num_tokens=50):
+		self._has_gpu = torch.cuda.device_count() > 0
+		self._model = BertVector
