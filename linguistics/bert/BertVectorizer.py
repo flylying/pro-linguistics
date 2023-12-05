4ebb0cdf67ce24df693599c35a5dfe21a89cb6b4
flylying <145457728+flylying@users.noreply.github.com>
2023-12-05 20:17:44 +0000

    more pep8 fixes

--- linguistics/bert/BertVectorizer.py
+++ linguistics/bert/BertVectorizer.py
@@ -5,4 +5,8 @@ import torch
 class BertVectorizer:
 	def __init__(self, num_tokens=50):
 		self._has_gpu = torch.cuda.device_count() > 0
-		self._model = BertVector
+		self._model = BertVectorizerModel(num_tokens=num_tokens)
+
+		if self._has_gpu:
+			self._device = torch.device('cuda')
+			self._num_devices = 
