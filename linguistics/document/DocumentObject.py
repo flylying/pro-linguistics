41f65999988857496165e4da38d1e55f4e474d1a
flylying <145457728+flylying@users.noreply.github.com>
2023-12-24 12:03:50 +0000

    code review changes

--- linguistics/document/DocumentObject.py
+++ linguistics/document/DocumentObject.py
@@ -6,4 +6,12 @@ class DocumentObject:
 		:param obj: any object from a document
 		:type document: Document
 		"""
-	
+		self._obj = obj
+		self._document = document
+
+	@property
+	def nlp(self):
+		return self.document.nlp
+
+	@property
+	def document(self)
