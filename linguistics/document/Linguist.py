25a81077a311464a513ead3b191d506de5c0c3e2
flylying <145457728+flylying@users.noreply.github.com>
2024-01-17 17:27:09 +0000

    add more

--- linguistics/document/Linguist.py
+++ linguistics/document/Linguist.py
@@ -5,4 +5,8 @@ from .Document import Document
 
 
 class Linguist:
-	def __init__(self, spacy_core='en_core_web_sm', prefer_g
+	def __init__(self, spacy_core='en_core_web_sm', prefer_gpu=None, wikipedia_api=None):
+		if prefer_gpu is None:
+			self._gpu = spacy.prefer_gpu()
+		elif prefer_gpu:  # explicit True
+			self._gp
