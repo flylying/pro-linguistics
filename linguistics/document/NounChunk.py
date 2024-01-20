2914302fcb4253cb03ae60fa29120ecebbae4b86
flylying <145457728+flylying@users.noreply.github.com>
2024-01-20 19:49:49 +0000

    update migration

--- linguistics/document/NounChunk.py
+++ linguistics/document/NounChunk.py
@@ -1,2 +1,8 @@
 from .TokenSpan import TokenSpan
-from .remove_list_duplicates im
+from .remove_list_duplicates import remove_list_duplicates
+from .Entity import Entity
+
+
+class NounChunk(TokenSpan):
+
+	def __init__(self, obj, documen
