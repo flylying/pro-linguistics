e7d903688214cb992350101385b8e9c804cbb9bb
flylying <145457728+flylying@users.noreply.github.com>
2024-01-22 08:33:39 +0000

    Fix formatting in README.md

--- linguistics/document/NounChunk.py
+++ linguistics/document/NounChunk.py
@@ -5,4 +5,10 @@ from .Entity import Entity
 
 class NounChunk(TokenSpan):
 
-	def __init__(self, obj, documen
+	def __init__(self, obj, document):
+		super().__init__(obj=obj, document=document)
+		for token in self.tokens:
+			token._noun_chunk = self
+
+	@property
+	def child_entitie
