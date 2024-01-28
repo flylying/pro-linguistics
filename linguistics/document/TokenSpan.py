c3f1bb20b304ecf58ebbd3974b80b03b4f88a53d
flylying <145457728+flylying@users.noreply.github.com>
2024-01-28 19:35:13 +0000

    Misc. changes

--- linguistics/document/TokenSpan.py
+++ linguistics/document/TokenSpan.py
@@ -2,4 +2,8 @@ from .DocumentObject import DocumentObject
 from spacy import tokens
 
 
-class TokenSpan(Do
+class TokenSpan(DocumentObject):
+
+	@property
+	def character_range(self):
+		return self.tokens[0].character_range[0], self.tokens[-1].character_ran
