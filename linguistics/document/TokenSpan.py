c2472cbb9fb8bbfc3e22e151b7912996da9b69b6
flylying <145457728+flylying@users.noreply.github.com>
2024-02-01 12:00:17 +0000

    updated regex

--- linguistics/document/TokenSpan.py
+++ linguistics/document/TokenSpan.py
@@ -6,4 +6,8 @@ class TokenSpan(DocumentObject):
 
 	@property
 	def character_range(self):
-		return self.tokens[0].character_range[0], self.tokens[-1].character_ran
+		return self.tokens[0].character_range[0], self.tokens[-1].character_range[1]
+
+	def __str__(self):
+		character_range = self.character_range
+		return self.document.text[character_range[0
