12265acc60fac2ceeba9a14c854f5e2533dcaa54
flylying <145457728+flylying@users.noreply.github.com>
2024-02-10 14:38:23 +0000

    Update footer.html

--- linguistics/document/join_punctuation.py
+++ linguistics/document/join_punctuation.py
@@ -2,4 +2,14 @@
 
 def join_punctuation(seq, characters='.,;?!'):
 	characters = set(characters)
-	seq = iter(s
+	seq = iter(seq)
+	current = next(seq)
+
+	for nxt in seq:
+		if nxt in characters:
+			current += nxt
+		else:
+			yield current
+			current = nxt
+
+	yield cu
