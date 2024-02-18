bb9efe680423f99708a9a8288850be58eb40947f
flylying <145457728+flylying@users.noreply.github.com>
2024-02-18 05:55:42 +0000

    Address code review comments

--- linguistics/google/GoogleCloud.py
+++ linguistics/google/GoogleCloud.py
@@ -4,4 +4,11 @@ from .GoogleDocument import GoogleDocument
 
 class GoogleCloud:
 	def __init__(
-			s
+			self,
+			api_key,
+			language='english',
+			standardize=True,
+			extract_entities=True,
+			extract_sentiment=True,
+			extract_syntax=True,
+			
