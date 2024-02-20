8e2684155a21087ff41090e0af80c5f6a2abeaeb
flylying <145457728+flylying@users.noreply.github.com>
2024-02-20 19:29:17 +0000

    updating proposal data

--- linguistics/google/GoogleDocumentObject.py
+++ linguistics/google/GoogleDocumentObject.py
@@ -2,4 +2,7 @@ from slytherin import trim
 
 
 class GoogleDocumentObject:
-	def __ini
+	def __init__(self, dictionary, document, begin, end):
+		"""
+		:param dict dictionary: any object from a document
+		:type document: .GoogleDo
