f2b2a36eca55ef4e029bfee203959d3b9ee975af
flylying <145457728+flylying@users.noreply.github.com>
2024-03-02 20:22:53 +0000

    Update exp.jl

--- linguistics/google/GoogleToken.py
+++ linguistics/google/GoogleToken.py
@@ -2,4 +2,8 @@ from .GoogleDocumentObject import GoogleDocumentObject
 
 
 class GoogleToken(GoogleDocumentObject):
-	def __init__(self, diction
+	def __init__(self, dictionary, document):
+		text = dictionary.pop('text')
+		content = text.pop('content')
+		begin = text.pop('begin_offset')
+		end = beg
