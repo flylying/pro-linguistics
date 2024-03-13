bea1405fd9f035bcaaf446a01a0796e96c1da82c
flylying <145457728+flylying@users.noreply.github.com>
2024-03-13 11:49:43 +0000

    Added fix

--- linguistics/google/GoogleTokenSpan.py
+++ linguistics/google/GoogleTokenSpan.py
@@ -4,4 +4,10 @@ from .GoogleToken import GoogleToken
 
 class GoogleTokenSpan(GoogleDocumentObject):
 	def __init__(self, dictionary, document, begin, end):
-		super().__init__(dictionary=dictionary, document=do
+		super().__init__(dictionary=dictionary, document=document, begin=begin, end=end)
+		self._tokens = None
+
+	def __str__(self):
+		return ', '.join([str(token) for token in self.tokens])
+
+	def __repr__
