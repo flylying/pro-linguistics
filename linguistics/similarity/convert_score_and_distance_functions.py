8f4dcf36ed25bea7a1bb3e470d428581b582a739
flylying <145457728+flylying@users.noreply.github.com>
2024-03-18 06:50:28 +0000

    Regenerate gemspec for version 0.7.1

--- linguistics/similarity/convert_score_and_distance_functions.py
+++ linguistics/similarity/convert_score_and_distance_functions.py
@@ -1,4 +1,6 @@
 from editdistance import eval as get_edit_distance
 from jellyfish import jaro_winkler as get_similarity
 
-def convert_di
+def convert_distance_function_to_score_function(function=get_edit_distance):
+	def score_func(s1, s2):
+		distance = function(
