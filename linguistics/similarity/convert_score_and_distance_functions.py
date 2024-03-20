7c070352d0a49eb05384bf4cec2a70f9bb619991
flylying <145457728+flylying@users.noreply.github.com>
2024-03-20 13:52:52 +0000

    Update logger.js

--- linguistics/similarity/convert_score_and_distance_functions.py
+++ linguistics/similarity/convert_score_and_distance_functions.py
@@ -3,4 +3,9 @@ from jellyfish import jaro_winkler as get_similarity
 
 def convert_distance_function_to_score_function(function=get_edit_distance):
 	def score_func(s1, s2):
-		distance = function(
+		distance = function(s1, s2)
+		score = max(0,1-float(distance)/max(len(s1), len(s2)))
+		return score
+	return score_func
+
+def create_weight
