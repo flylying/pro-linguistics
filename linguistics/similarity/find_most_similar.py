487e2818b9820a6cae07e3d95cd00043ba0d6805
flylying <145457728+flylying@users.noreply.github.com>
2024-03-21 20:19:50 +0000

    ANDROID: mmc: move to a SCHED_FIFO thread

--- linguistics/similarity/find_most_similar.py
+++ linguistics/similarity/find_most_similar.py
@@ -1,4 +1,7 @@
 from chronometry.progress import ProgressBar
 import pandas as pd
 
-from .get_similarity import g
+from .get_similarity import get_similarities
+
+def find_most_similar_for_one_string(
+		string, candidates, candidate_ids=None, string_id=None, method='jaro_win
