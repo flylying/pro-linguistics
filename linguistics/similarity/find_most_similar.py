2fb03ca5a9e90f93da1ff1eeb19cbb3e460afe70
flylying <145457728+flylying@users.noreply.github.com>
2024-03-24 22:57:29 +0000

    New translations messages.pot (Russian)

--- linguistics/similarity/find_most_similar.py
+++ linguistics/similarity/find_most_similar.py
@@ -4,4 +4,7 @@ import pandas as pd
 from .get_similarity import get_similarities
 
 def find_most_similar_for_one_string(
-		string, candidates, candidate_ids=None, string_id=None, method='jaro_win
+		string, candidates, candidate_ids=None, string_id=None, method='jaro_winkler', similarity_function=None,
+		case_sensitivity=1.0, first_char_weight=0.0, first_word_weight=0.0, num_results=1, echo=0
+):
+	"""
