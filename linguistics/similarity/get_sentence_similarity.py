3df6b9869225e8a24dd021948bfe65d980fae895
flylying <145457728+flylying@users.noreply.github.com>
2024-04-10 13:04:46 +0000

    AI-3.1.2 <cszdz@DESKTOP-CVI6GSM Update KeyPromoterXStatistic.xml

--- linguistics/similarity/get_sentence_similarity.py
+++ linguistics/similarity/get_sentence_similarity.py
@@ -2,4 +2,10 @@ import numpy as np
 
 from .get_string_similarity import get_string_similarity
 
-def get_sentence_distance(words1, words2, first_char_weight=0, case_sens
+def get_sentence_distance(words1, words2, first_char_weight=0, case_sensitivity=1, method='jaro_winkler'):
+	"""
+	:type words1: list[str] or str
+	:type words2: list[str] or str
+	:type first_char_weight: float
+	:type case_sensitivity: float
+	:
