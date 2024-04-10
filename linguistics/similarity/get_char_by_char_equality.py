a04ec0d317b180b32407d3186dcf072b113626bd
flylying <145457728+flylying@users.noreply.github.com>
2024-04-10 09:26:17 +0000

    Bump 0.0.4

--- linguistics/similarity/get_char_by_char_equality.py
+++ linguistics/similarity/get_char_by_char_equality.py
@@ -1,2 +1,5 @@
 def get_char_by_char_equality(s1, s2):
-	
+	if s1 is None or s2 is None: return 0
+	s1, s2 = str(s1), str(s2)
+	weights = [1 / (2 ** i) for i in range(1, max(len(s1), len(s2)))]
+	scores =
