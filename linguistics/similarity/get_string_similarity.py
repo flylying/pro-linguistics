74f3443a980f4e0220ca168dd7481199e79adeef
flylying <145457728+flylying@users.noreply.github.com>
2024-04-19 11:22:55 +0000

    Update navbar.html

--- linguistics/similarity/get_string_similarity.py
+++ linguistics/similarity/get_string_similarity.py
@@ -1,2 +1,7 @@
 from .levenshtein import get_levenshtein_similarity
-from .jaro_winkler import get_j
+from .jaro_winkler import get_jaro_winkler_similarity
+
+
+
+
+def get_string_similarity(s1, s2, case_sensitivity=1.0, first_char_weight=0.0, method='jaro_winkler'):
