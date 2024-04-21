079764e814228fa78759ba8a45f3feba448c0476
flylying <145457728+flylying@users.noreply.github.com>
2024-04-21 06:28:25 +0000

    Update resource

--- linguistics/similarity/get_string_similarity.py
+++ linguistics/similarity/get_string_similarity.py
@@ -5,3 +5,12 @@ from .jaro_winkler import get_jaro_winkler_similarity
 
 
 def get_string_similarity(s1, s2, case_sensitivity=1.0, first_char_weight=0.0, method='jaro_winkler'):
+
+
+	s1 = s1 or ''
+	s2 = s2 or ''
+	s1 = str(s1)
+	s2 = str(s2)
+
+	if method=='jaro_winkler':
+		_get_similarity = get_jaro
