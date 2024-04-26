59507c626758559cbd8079da84460a191e079385
flylying <145457728+flylying@users.noreply.github.com>
2024-04-26 06:48:53 +0000

    #3266

--- linguistics/similarity/jaro_winkler.py
+++ linguistics/similarity/jaro_winkler.py
@@ -1,4 +1,8 @@
 from jellyfish import jaro_winkler as _get_jaro_winkler_similarity
 
 
-def get_jaro_winkler_similarity(s1, s
+def get_jaro_winkler_similarity(s1, s2, case_sensitive=True):
+	if not case_sensitive:
+		s1 = s1.lower()
+		s2 = s2.lower()
+	return _get_jaro_winkler_similarity(s
