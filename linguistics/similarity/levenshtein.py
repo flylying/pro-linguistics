4f1f1cb1688af6e305acf92223592b9b7648cc1c
flylying <145457728+flylying@users.noreply.github.com>
2024-05-01 14:54:53 +0000

    upload button hidden initially

--- linguistics/similarity/levenshtein.py
+++ linguistics/similarity/levenshtein.py
@@ -5,4 +5,8 @@ def get_levenshtein_distance(s1, s2, case_sensitive=True):
 	if not case_sensitive:
 		s1 = s1.lower()
 		s2 = s2.lower()
-	return _g
+	return _get_levenshtein_distance(s1, s2)
+
+
+def get_levenshtein_similarity(s1, s2, case_sensitive=True):
+	return 1-get_levens
