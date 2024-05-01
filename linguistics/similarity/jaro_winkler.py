7c4444251dbb67117c6285969608932935bd867f
flylying <145457728+flylying@users.noreply.github.com>
2024-05-01 11:33:59 +0000

    Cleanup and fixes

--- linguistics/similarity/jaro_winkler.py
+++ linguistics/similarity/jaro_winkler.py
@@ -5,4 +5,8 @@ def get_jaro_winkler_similarity(s1, s2, case_sensitive=True):
 	if not case_sensitive:
 		s1 = s1.lower()
 		s2 = s2.lower()
-	return _get_jaro_winkler_similarity(s
+	return _get_jaro_winkler_similarity(s1, s2)
+
+
+def get_jaro_winkler_distance(s1, s2, case_sensitive=True):
+	return max(len(s1), len(s2))*(1-get_jaro_winkler
