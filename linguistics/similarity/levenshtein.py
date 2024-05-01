166e233511dacaaf5ba443a61058ef9e0d94349a
flylying <145457728+flylying@users.noreply.github.com>
2024-05-01 13:58:08 +0000

    f2fs: throttle the memory footprint with a sysfs entry

--- linguistics/similarity/levenshtein.py
+++ linguistics/similarity/levenshtein.py
@@ -1,4 +1,8 @@
 from editdistance import eval as _get_levenshtein_distance
 
 
-d
+def get_levenshtein_distance(s1, s2, case_sensitive=True):
+	if not case_sensitive:
+		s1 = s1.lower()
+		s2 = s2.lower()
+	return _g
